550ad811ee0a02838d4aad37729063e90d8fc584
Denise Shamira <shamiraxp10@gmail.com>
2025-11-17 21:55:59 -0300

    tentando implementar o botao de dark-mode

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./docs/**/*.{html,js}",
+  ],  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
